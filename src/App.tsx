8732c10870583fbcae25c233d6cf31485ef1168c
Le_Vrai_WIWI <superwoup@gmail.com>
2024-06-29 15:52:11 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-9xl mt-10">Vite + React + tailwind</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
